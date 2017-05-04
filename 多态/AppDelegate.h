2d31241fc8f9513669bbb101906cbafd47f2abdc
zs <798506095@qq.com>
2017-05-04 14:53:05 +0800

    Initial Commit

---
+++ 多态/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多态
+//
+//  Created by wordy on 2017/5/4.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
